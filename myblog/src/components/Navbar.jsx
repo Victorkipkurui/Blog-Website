97344c563a8d1509bd83900dae03c932ec6f22a8
Victor Kipkurui <kipkurui.victor18@students.dkut.ac.ke>
2024-09-09 12:51:24 +0300

    Navbar

---
+++ myblog/src/components/Navbar.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { FaGithub, FaXTwitter, FaLinkedin, FaBars, FaXmark} from "react-icons/fa6";
+
+const Navbar = () => {
+  const [isMenuon, setIsmenuon] = useState(false);
+
+  const toggleMenu = ()=>{
+    setIsmenuon(!isMenuon);
+  }
+  const navItems = [
+    {path:'/', link:"Home"},
+    {path:'/about', link:"About"},
+    {path:'/blogs', link:"Blogs"},
+    {path:'/services', link:"Services"},
+    {path:'/contacts', link:"Contacts"}
+  ]
+  return (
+   <header className='bg-black text-white fixed top-0 right-0 left-0'>
+
+    {/*large devices*/}
+    <nav className='px-4 py-4 max-w-7xl mx-auto flex justify-between items-center'>
+
+      <a href="/" className='text-xl font-bold text-white'>Victor<span className='text-orange-600'>BS</span></a>
+
+      <ul className='md:flex gap-12 text-lg hidden'>
+        {
+          navItems.map(({path, link}) => <li className='text-white' key={path}>
+            <NavLink to={path}>{link}</NavLink>
+          </li>)
+        }
+      </ul>
+      <div className='text-white lg:flex gap-4 items-center hidden'>
+        <a href="/" className='hover:text-orange-600'><FaGithub></FaGithub></a>
+        <a href="/" className='hover:text-orange-600'><FaXTwitter></FaXTwitter></a>
+        <a href="/" className='hover:text-orange-600'><FaLinkedin></FaLinkedin></a>
+        <button className='bg-orange-600 px-6 py-2 font-medium rounded hover:bg-white hover:text-orange-600 transition-all duration-200 ease-in'>Log In</button>
+      </div>
+      <div className='md:hidden'>
+        <button onClick={toggleMenu} className='cursor-pointer'>
+          { isMenuon ? <FaXmark className='w-5 h-5'></FaXmark> :<FaBars className='w-5 h-5'></FaBars>}</button>
+      </div>
+    </nav>
+    {/* Menu on mobile*/}
+    <div>
+    <ul className={`md:hidden gap-12 text-lg block space-y-4 px-4 py-6 mt-14 bg-white ${isMenuon ? 'fixed top-0 left-0 w-full transition-all ease-out duration-150' : 'hidden'}`}>
+        {
+          navItems.map(({path, link}) => <li className='text-black' key={path}>
+            <NavLink onClick={toggleMenu} to={path}>{link}</NavLink>
+          </li>)
+        }
+      </ul>
+    </div>
+   </header>
+  )
+}
+
+export default Navbar
---
+++ myblog/src/main.jsx
@@ -0,0 +1,42 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home.jsx';
+import Blogs from './pages/Blogs.jsx';
+import About from './pages/About.jsx';
+import Contacts from './pages/Contacts.jsx';
+import Services from './pages/Services.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children:[{
+      path: '/',
+      element: <Home></Home>
+    },{
+      path: '/about',
+      element: <About></About>
+    },{
+      path: '/blogs',
+      element: <Blogs></Blogs>
+    },{
+      path: '/services',
+      element: <Services></Services>
+    },{
+      path: '/contacts',
+      element: <Contacts></Contacts>
+    }]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
